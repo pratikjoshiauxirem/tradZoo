2c37bb1ac1797b79462320267c027f1da76ce1bd
pratikjoshiauxirem <pratikjoshi@auxirem.com>
2022-10-11 09:23:39 +0530

    init
    Signed-off-by: pratikjoshiauxirem <pratikjoshi@auxirem.com>

---
+++ public/tradZoo/app/Views/index/intro.php
@@ -0,0 +1,172 @@
+<link rel="stylesheet" href="assets/css/style.css">
+<div class="container-fluid p-0 m-0" style="width:100vw; background-color:rgb(240, 275, 232); height:100%">
+<div class="container">
+        <nav class="p-2 w-50 m-auto" >
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <img src="assets/images/images/logo.webp" class="w-100">
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-6 m-auto">
+                        <h1>TradZoo</h1>
+                        <p class="lead">Platform for Traders & Investors</p>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="about text-center p-5">
+                <h2 >About Us</h2>
+                <p class="font-25">
+                    Our Mission at TradZoo is to help people learn financial  stability and financial freedom.
+                    Designed to provide you with daily  free Swing & Intraday trading analysis which would greately help you with the timely buy,sell and learn,
+                    while growing both knowledge and capital.
+                </p>
+              
+        </div>
+        <div class="carousel text-center m-0 p-5">
+                <h2  class="pb-5">App Screenshots</h2>
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" id="apps">
+                <div class="carousel-inner" id="aci" style="overflow-x:hiden;">
+    <div class="carousel-item" id="pratik">
+      <img class="d-block w-100" src="assets/images/images/s1.webp" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/images/s2.webp" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/images/s3.webp" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/images/s4.webp" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/images/s5.webp" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/images/s6.webp" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/images/s7.webp" alt="Third slide">
+    </div>
+   
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" onclick="minus()" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" onclick="plus()" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+                </div>
+        </div>
+        <div class="carousel1 text-center m-0 p-5">
+                <h2 style="overflow:hidden;" class="pb-5">What People says About Us</h2>
+                <div id="review" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner" id="r1">
+                    <div class="carousel-item active " id="r2">
+                    <img class="d-block w-100" src="assets/images/images/reviews/1.webp" alt="First slide">
+                   
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="assets/images/images/reviews/2.webp" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="assets/images/images/reviews/3.webp" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" onclick="minus1()" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" onclick="plus1()" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                </div>
+        </div>
+        <div class="container p-5">
+            <center>
+                <h3>Download Now for Free</h3>
+                <h4 class="text-success pb-2">Use Code TRADZOO to get 20% cashback on your first purchase</h4>
+             
+                   <div class="row w-50">
+                            <div class="col-md-6 col-xs-12 col-sm-12">
+                               <div class="container-fluid row bg-white rounded border">
+                                 <div class="col-md-6 col-sm-6 col-xs-6">
+                                    <img src="assets/images/images/icons/playstore.png" class="w-100 p-1">
+                                 </div>
+                                 <div class="col-md-6 col-sm-6 col-xs-6">
+                                    <h6 class="p-0 m-0">Android Play Store</h6>
+                                 </div>
+                               </div>
+                               
+                            </div>
+                            <div class="col-md-6 col-xs-12 col-sm-12">
+                               <div class="container-fluid row bg-white rounded border">
+                                 <div class="col-md-6 col-sm-6 col-xs-6">
+                                    <img src="assets/images/images/icons/appstore.webp" class="w-100 p-1">
+                                 </div>
+                                 <div class="col-md-6 col-sm-6 col-xs-6">
+                                    <h6 class="p-0 m-0">Apple App Store</h6>
+                                 </div>
+                               </div>
+                               
+                            </div>
+                   </div>
+                <div class="p-2">
+                     <h3 class="pb-5">Other Social Media Channels</h3>
+                    <ul type="none" class="ulh">
+                        <li class="">
+                            <a href=""><p><span class="fa fa-youtube"></span><br>Youtube</p></a>
+                        </li>
+                        <li class="">
+                            <a href=""><p><img src="assets/images/images/icons/tradingview.webp" style="height:17px;"><br>TradingView</p></a>
+                        </li>
+                        <li class="">
+                            <a href=""><p><span class="fa fa-telegram"></span><br>Telegram</p></a>
+                        </li>
+                        <li class="">
+                            <a href=""><p><span class="fa fa-twitter"></span><br>twitter</p></a>
+                        </li>
+                        <li class="">
+                            <a href=""><p><span class="fa fa-instagram"></span><br>Instagram</p></a>
+                        </li>
+                        <li class="">
+                            <a href=""><p><span class="fa fa-facebook"></span><br>Facebook</p></a>
+                        </li>
+                        <li class="">
+                            <a href=""><p><span class="fa fa-whatsapp"></span><br>Whatsapp</p></a>
+                        </li>
+                        <li class="">
+                            <a href=""><p><span class="fa fa-desktop"></span><br>Website</p></a>
+                        </li>
+                    </ul>
+                    <h4 class="text-success pt-5 w-75">Join Now and start your financial journey with TradZoo  Trusted by 40k+ users</h4>
+                </div>
+            </center>
+        </div>
+
+    </div>
+    <div class="container-fluid">
+        <div class="row w-75 m-auto">
+            <div class="col-md-6 col-xs-12 col-sm-12">
+                    <img src="assets/images/images/telegram.webp" class="w-100">
+            </div>
+            <div class="col-md-6 col-xs-12 col-sm-12 m-auto text-center">
+                    <p class="font-weight-bold " style="font-size:150%;">Join TradZoo Telegram Community</p>
+                    <p>Become a part of our Tradzoo's Telegram community and  stay updated with all the market updates and offers  Stay tuned for the most profitable trades.</p>
+                    <div class="row w-50 p-1 m-auto border bg-white rounded" >
+                        <div class="col-md-6 col-xs-6 col-sm-6">
+                            <img src="assets/images/images/icons/telegramicon.webp" class="w-100">
+                        </div>
+                        <div class="col-md-6 col-xs-6 col-sm-6 m-auto">
+                            <h5>Telegram Community</h5>
+                        </div>
+                    </div>
+                </div>
+           
+        </div>
+
+    </div>
+</div>
